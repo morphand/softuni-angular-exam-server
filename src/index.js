b62c30bf0d3fc7bb3ea3d79e76d6a86a62e0f682
morphand <75131830+morphand@users.noreply.github.com>
2023-05-14 15:19:40 +0300

    add controllers

--- src/index.js
+++ src/index.js
@@ -4,20 +4,24 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 // Controllers
+const loginController = require("./controllers/login");
+const registerController = require("./controllers/register");
+const logoutController = require("./controllers/logout");
 
 const { PORT, DB_NAME } = require("./constants");
 
 // Middlewares
-const authToken = require("./middlewares/authToken");
+const authTokenMiddleware = require("./middlewares/authToken");
 
 // Init
 const app = express();
+const authCache = new Map();
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
 app.use("/static", express.static("static"));
-app.use(authToken);
+app.use(authTokenMiddleware);
 
 mongoose.connect(`mongodb://127.0.0.1/${DB_NAME}`).then(() => {
   console.log(`Connected to database ${DB_NAME}.`);
@@ -25,3 +29,12 @@ mongoose.connect(`mongodb://127.0.0.1/${DB_NAME}`).then(() => {
     console.log(`Server listening on port ${PORT}.`);
   });
 });
+
+// Login
+app.post("/login", loginController(authCache));
+
+// Register
+app.post("/register", registerController(authCache));
+
+// Logout
+app.post("/logout", logoutController);
