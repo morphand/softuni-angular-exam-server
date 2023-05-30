76f41b747a29aec020493ab2b0bb6b1a354f3efd
morphand <75131830+morphand@users.noreply.github.com>
2023-05-30 18:00:39 +0300

    add error handling to the authToken middleware

--- src/middlewares/authToken.js
+++ src/middlewares/authToken.js
@@ -7,17 +7,32 @@ const authService = require("../services/auth");
 // Constants
 const { JWT_SECRET } = require("../constants");
 
+// Utils
+const Result = require("../utils/Result");
+
 async function requiresToken(req, res, next) {
   let token = req.headers.authentication;
-  if (token) {
+  const errorResult = new Result({
+    success: false,
+    errors: ["Invalid JSON web token."],
+  });
+
+  if (!token) {
+    return res.json(errorResult);
+  }
+
+  try {
     token = token.replace("Bearer: ", "");
     const decodedToken = await jwt.verify(token, JWT_SECRET);
     const userId = decodedToken._id;
     const username = decodedToken.username;
     const isUserAdmin = await authService.isUserAdmin(userId);
     req.token = { userId, username, isUserAdmin };
+    return next();
+  } catch (e) {
+    console.error(e);
+    return res.json(errorResult);
   }
-  return next();
 }
 
 module.exports = requiresToken;
